3c129fa8ff20ce6810027f233c4d2027e078da98
Phantom Daldust <phantom.daldust@gmail.com>
2022-08-29 22:00:35 -0400

    Fixed incorrect variable names

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
-import { darkMode, lightMode } from './features/modeSlice'
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import {fetchData, loadData, nextImage, prevImage, setArtId, reset} from './features/dataSlice'
 
-function App() {
+import { useEffect } from 'react';
+
+const mapStateToProps = (state) => ({
+  artId: state.data.artId
+})
+
+function App(props) {
   const dispatch = useDispatch()
-  const mode = useSelector((state) => state.mode)
+  const data = useSelector((state) => state.data)
 
-  const toggleMode = () => {
-    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())
+  useEffect(() => {
+    dispatch(fetchData())
+  },[props.artId, dispatch])
+
+    const renderImg = () => {
+    if(data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>image here</p>
+    }
   }
-  
+
   return (
-    <div style={{ backgroundColor: mode.color1, color: 'white' }} className="App">
-      <Nav />
-      <button onClick={toggleMode}>{ mode.darkMode ? 'Light Mode' : 'Dark Mode' }</button>
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(reset())}>Clear</button>
+        <button onClick={() => dispatch(nextImage())}>Next</button>
+        <button onClick={() => dispatch(prevImage())}>Back</button>
+      </div>
+      <input value={ data.artId } onChange={(e) => {
+        dispatch(setArtId(Number(e.target.value)))
+      }} />
+      <div>
+        {data.artId}
+        {renderImg()}
+      </div>
     </div>
+
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
+
